2d72266d742a274b4dd4fc737cab620b43b8ea4d
DenisGitHubs <128564592+DenisGitHubs@users.noreply.github.com>
2023-12-13 22:29:28 +0700

    12

--- src/helpers/sign.js
+++ src/helpers/sign.js
@@ -1,5 +1,5 @@
-import { singIn } from "../Api/userApi";
-import { saveTokenUserAfterSignIn } from "../Store/Slices/userSlice";
+import { registerUser, singIn } from "../Api/userApi";
+import { saveTokenUserAfterSignIn, saveUserAfterReg } from "../Store/Slices/userSlice";
 
 export const handleEmail = (setEmail, event) => {
     setEmail(event.target.value);
@@ -22,4 +22,9 @@ export const handleCity = (setCity, event) => {
 export const handleSignIn = async (email, password, dispatch) => {
     const data = await singIn(email, password);
     dispatch(saveTokenUserAfterSignIn({data}))
+  }
+export const saveAndRegisterUser = async (email, password, name, role, surname, city, dispatch) => {
+    const data = await registerUser(email, password, name, role, surname, city);
+    dispatch(saveUserAfterReg({data}))
+    handleSignIn(email, password, dispatch);
   }
