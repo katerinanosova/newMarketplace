05aa8d9b8b65640ddeaa24f5277388131ff6b347
DenisGitHubs <128564592+DenisGitHubs@users.noreply.github.com>
2023-12-20 16:02:21 +0700

    Revert "Dev"

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import { Main } from './Pages/Main/main';
 import { Profile } from './Pages/Profile/Profile';
 import { SellerProfile } from './Pages/SellerProfile/SellerProfile';
 import { Sign } from './Pages/Registration/sign';
 import { Product } from './Pages/Product/Product';
+import { Review } from './Components/reviews/review';
 import { NewProduct } from './Components/NewProductAdd/newProduct';
+import { EditorAdv } from './Components/EditorAdv/editor';
 import { NotFound } from './Pages/NotFound/NotFound';
-import { ProtectedRoute } from './ProtectedRoutes'
 
 const products = [
   {
@@ -40,19 +42,20 @@ const products = [
 ];
 
 export const AppRoutes = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
 
   return (
     <Routes>
       <Route path='/' element={<Main products={products} />} />
+      <Route path='/profile' element={<Profile products={products} />} />
+      <Route path='/seller-profile' element={<SellerProfile products={products} />} />
+      <Route path='/product/:id' element={<Product products={products} /> } />
+      {/* <Route path='/review' element={<Review />} /> */}
+      <Route path='/new-product' element={<NewProduct />} />
       <Route path='/reg' element={<Sign />} />
+      {/* <Route path='/editor-adv' element={<EditorAdv setIsOpen={setIsOpen} />} /> */}
       <Route path='*' element={<NotFound />} />
-
-      <Route element={<ProtectedRoute /> }>
-          <Route path='/profile' element={<Profile products={products} />} />
-          <Route path='/seller-profile' element={<SellerProfile products={products} />} />
-          <Route path='/product/:id' element={<Product products={products} /> } />
-          <Route path='/new-product' element={<NewProduct />} />
-      </Route>
     </Routes>
   );
 };
